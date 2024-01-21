e742adcc35062ecb9c58e239ab675afa16bd1400
Shuhuai Lin <68037461+Linshuhuai@users.noreply.github.com>
2024-01-20 22:45:34 -0800

    Create api_gpt3.py

---
+++ code/api_gpt3.py
@@ -0,0 +1,38 @@
+import openai
+import requests
+openai.api_key = ""  # replace with your api_key
+ApiKey = ""  # replace with your api_key
+
+
+def get_response(model, prompt):
+    """Get response from different OpenAI models."""
+    if model == "gpt-3.5-turbo":
+        h = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {ApiKey}'
+        }
+        d = {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": prompt}],
+            "max_tokens": 100,
+            "temperature": 0
+        }
+        u = 'https://api.openai.com/v1/chat/completions'
+        r = requests.post(url=u, headers=h, json=d).json()
+        return r["choices"][0]["message"]["content"].strip()
+
+    elif model == "text-davinci-003":
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.6
+        )
+        return response["choices"][0]["text"].strip()
+
+    elif model == "text-davinci-002":
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=prompt,
+            temperature=0.6
+        )
+        return response["choices"][0]["text"].strip()
