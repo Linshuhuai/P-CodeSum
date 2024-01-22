d35b6c8ab088e1dd1537eb9f5fca3e8906ed4bba
yunshangbo <2198192688@qq.com>
2024-01-22 16:53:47 +0800

    [YSB] modify generate dataset.py

--- code/generate_dataset.py
+++ code/generate_dataset.py
@@ -11,7 +11,7 @@ openai.api_key = ""
 input_dir = "../data/"
 output_dir = "../data/"
 model = ["gpt-3.5-turbo", "text-davinci-003", "text-davinci-002"]
-dataset = 'h2o-3'
+dataset = "h2o-3"
 # dataset = ['airflow', 'geopackage-android', 'grimoirelab-perceval', 'h2o-3', 'jain-slee', 'probability', 'wildfly']
 description_dict = [
     "# An elaborate, high quality docstring for the above function: ",
@@ -21,19 +21,24 @@ description_dict = [
     "# Short, project-specific code summarization that accurately captures the essence of above code\\n# ",
     "# Overview of the main functionality and purpose of the above code in a specific code project\\n# ",
     "",
-    "# Here‘s a function and what the function is doing: ",
-    "Can you generate high quality docstring for the above function?"
+    "# Here's a function and what the function is doing: ",
+    "Can you generate high quality docstring for the above function?",
 ]
 
 rouge_calculator = Rouge()
 bleu_total = []
 rouge_total = []
 
-examples = read_examples(input_dir + dataset + '/examples_20.jsonl', 20, 'summarize')
-queries = read_examples(input_dir + dataset + '/train_30.jsonl', 30, 'summarize')
+examples = read_examples(input_dir + dataset + "/examples_20.jsonl", 20, "summarize")
+queries = read_examples(input_dir + dataset + "/train_30.jsonl", 30, "summarize")
 
 for idx in range(0, 30):
-    fw = open(os.path.join('../data/h2o-3/text-davinci-003/data_' + format(idx) + '_5_25.jsonl'), 'w')
+    fw = open(
+        os.path.join(
+            "../data/h2o-3/text-davinci-003/data_" + format(idx) + "_5_25.jsonl"
+        ),
+        "w",
+    )
     bleu_q = []
     rouge_q = []
     prompt_q = queries[idx].source + description_dict[0]
@@ -43,7 +48,13 @@ for idx in range(0, 30):
         prompt = ""
         selected_examples = choice(5, example_list)
         for example_idx in selected_examples:
-            prompt = prompt + examples[example_idx].source + description_dict[0] + examples[example_idx].target + '\n'
+            prompt = (
+                prompt
+                + examples[example_idx].source
+                + description_dict[0]
+                + examples[example_idx].target
+                + "\n"
+            )
 
         # send request to OpenAI
         hypothesis = get_response(model[1], prompt + prompt_q).replace("\n", "\\n")
@@ -52,12 +63,25 @@ for idx in range(0, 30):
         # Compute ROUGE scores
         rouge = rouge_calculator.calc_score([hypothesis], [reference])
         # write idx and bleu/rouge to files
-        fw.write(format(idx) + " " + format(selected_examples) + " " + format(bleu) + " " + format(rouge) + "\n")
+        fw.write(
+            format(idx)
+            + " "
+            + format(selected_examples)
+            + " "
+            + format(bleu)
+            + " "
+            + format(rouge)
+            + "\n"
+        )
         bleu_q.append(bleu)
         rouge_q.append(rouge)
         time.sleep(20)
 
-    fw.write("total: " + format(np.average(np.array(bleu_q))) + " " + format(np.average(np.array(rouge_q))) + "\n")
+    fw.write(
+        "total: "
+        + format(np.average(np.array(bleu_q)))
+        + " "
+        + format(np.average(np.array(rouge_q)))
+        + "\n"
+    )
     fw.close()
-
-
