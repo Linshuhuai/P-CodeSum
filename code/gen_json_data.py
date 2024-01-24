5f9b03bc99f375669b320a15b7f27c85791a27ee
yunshangbo <2198192688@qq.com>
2024-01-24 21:46:44 +0800

    [YSB] modify gen_json_data.py

--- code/gen_json_data.py
+++ code/gen_json_data.py
@@ -1,19 +1,22 @@
 import os
 import json
 
-input_data_dir = "../data/h2o-3/text-davinci-003/data_10/data_"
-output_data_dir = "./tmp.jsonl"
+datasets = ["omnibus", "Payum", "badger", "contracts", "h2o-3", "jain-slee"]
+
+input_data_dir = "./data/" + datasets[0] + "/text-davinci-003/data_5_25/"  # 待标记的中间结果
+output_data_dir = "./data/" + datasets[0] + "/text-davinci-003/tmp.jsonl"
 
 
 class ExampleIn(object):
     """A single training/test example."""
 
-    def __init__(self,
-                 query_idx=int,
-                 exp_idx=list[int],
-                 bleu=float,
-                 rouge=float,
-                 ):
+    def __init__(
+        self,
+        query_idx=int,
+        exp_idx=list[int],
+        bleu=float,
+        rouge=float,
+    ):
         self.query_idx = query_idx
         self.exp_idx = exp_idx
         self.bleu = bleu
@@ -23,11 +26,7 @@ class ExampleIn(object):
 class ExampleOut(object):
     """A single training/test example."""
 
-    def __init__(self,
-                 query_idx=int,
-                 example_idx=list[int],
-                 label=bool
-                 ):
+    def __init__(self, query_idx=int, example_idx=list[int], label=bool):
         self.query_idx = query_idx
         self.example_idx = example_idx
         self.label = label
@@ -37,7 +36,7 @@ def example_out_to_dict(out):
     return {
         "query_idx": out.query_idx,
         "example_idx": out.example_idx,
-        "label": out.label
+        "label": out.label,
     }
 
 
@@ -47,13 +46,13 @@ def read_example_in(filename, data_num):
     with open(filename, encoding="utf-8") as f:
         for idx, line in enumerate(f):
             if idx == data_num:
-                avg_bleu = float(line[line.index(':') + 2:].split()[0])
+                avg_bleu = float(line[line.index(":") + 2 :].split()[0])
                 break
-            query_idx = int(line[0:line.index('[') - 1])
-            arr = line[line.index('[') + 1:line.index(']')]
+            query_idx = int(line[0 : line.index("[") - 1])
+            arr = line[line.index("[") + 1 : line.index("]")]
             exp_idx = list(int(char) for char in arr.split(", "))
-            bleu = float(line[line.index(']') + 2:].split()[0])
-            rouge = float(line[line.index(']') + 2:].split()[1])
+            bleu = float(line[line.index("]") + 2 :].split()[0])
+            rouge = float(line[line.index("]") + 2 :].split()[1])
             examples.append(
                 ExampleIn(
                     query_idx=query_idx,
@@ -66,17 +65,19 @@ def read_example_in(filename, data_num):
 
 
 def main():
-    fw = open(os.path.join(output_data_dir), 'w')
-    for i in range(20, 30):
-        examples, avg_bleu = read_example_in(input_data_dir + format(i) + "_10_25.jsonl", 25)
+    fw = open(os.path.join(output_data_dir), "w")
+    for i in range(0, 30):
+        examples, avg_bleu = read_example_in(
+            input_data_dir + "data_" + format(i) + "_5_25.jsonl", 25
+        )
         for example in examples:
             exp = ExampleOut(
                 query_idx=example.query_idx,
                 example_idx=example.exp_idx,
-                label=True if example.bleu >= avg_bleu else False
+                label=True if example.bleu >= avg_bleu else False,
             )
             # write idx and bleu/rouge to files
-            fw.write(json.dumps(exp, default=example_out_to_dict) + '\n')
+            fw.write(json.dumps(exp, default=example_out_to_dict) + "\n")
     fw.close()
 
 
